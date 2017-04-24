466db6f291f169fa0fb6805ead4d85fe869baa05
min_li <309590999@qq.com>
2017-04-24 11:57:40 +0800

    啥同喜

---
+++ gitHubLearn/gitHubLearn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitHubLearn
+//
+//  Created by CHT on 17/4/24.
+//  Copyright © 2017年 chihaitao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gitHubLearn/gitHubLearnTests/gitHubLearnTests.m
@@ -0,0 +1,39 @@
+//
+//  gitHubLearnTests.m
+//  gitHubLearnTests
+//
+//  Created by CHT on 17/4/24.
+//  Copyright © 2017年 chihaitao. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface gitHubLearnTests : XCTestCase
+
+@end
+
+@implementation gitHubLearnTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
